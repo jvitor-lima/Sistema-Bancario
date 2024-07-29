239346bd911a68d672f86f0a60787b9915ca1e72
Rodrigo Santos <127608600+RodrigoSantos359@users.noreply.github.com>
2024-07-29 17:56:02 -0300

    Update usuario.py
    alteração

--- usuario.py
+++ usuario.py
@@ -1,27 +1,35 @@
+import re
+
 class Usuario:
     def __init__(self, nome, email, senha):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
+        self.set_nome(nome)
+        self.set_email(email)
+        self.set_senha(senha)
 
-    def get_nome(self):
-        return self.nome
 
     def set_nome(self, nome):
         self.nome = nome
 
+    def set_email(self, email):
+        if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            self.email = email
+        else:
+            raise ValueError("E-mail inválido")
+
+    def get_senha(self):
+        if len(senha) >= 8:
+            self.senha = senha
+        else:
+            raise ValueError("Senha deve ter pelo menos 8 caracteres")
+
+    def get_nome(self):
+        return self.nome
+
     def get_email(self):
         return self.email
-
-    def set_email(self, email):
-        self.email = email
 
     def get_senha(self):
         return self.senha
 
-    def set_senha(self, senha):
-        self.senha = senha
-
     def __str__(self):
         return f'Nome: {self.nome}, Email: {self.email}'
-    
